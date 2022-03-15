c9ee5bce24332988cdd9d299c103aeb2a72f9f68
origin <3225700692@qq.com>
2022-03-15 16:33:04 +0800

    1

--- src/main/java/person/otj/controller/StudentController.java
+++ src/main/java/person/otj/controller/StudentController.java
@@ -2,17 +2,31 @@ package person.otj.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/student")
 public class StudentController {
-    @RequestMapping("/findAll")
+    @RequestMapping(value = "/findAll")
     public String findAll(){
         return "studentList";
     }
 
-    @RequestMapping("/add")
-    public String add(){
-        return "studentAdd";
+    @RequestMapping(value = "/add",method ={ RequestMethod.POST,RequestMethod.GET})
+    public ModelAndView add(String username, String password){
+        ModelAndView modelAndView=new ModelAndView();
+        modelAndView.setViewName("studentList");
+        modelAndView.addObject("username",username);
+        modelAndView.addObject("password",password);
+        System.out.println(username);
+        System.out.println(password);
+        return modelAndView;
     }
+    @RequestMapping(value = "/test")
+    public String test(String data){
+        System.out.println(data);
+        return "test";
+    }
+
 }
